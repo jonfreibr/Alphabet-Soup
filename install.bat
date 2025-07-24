fd92cb0aacfc5baba141dd2b1ef54785b6186596
jonfreibr <jfreivald@brmedical.com>
2025-07-24 09:10:51 -0400

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,10 +2,10 @@
 echo Installing Python 3.11.5
 "%~dp0\python-3.11.5-amd64.exe" /passive
 echo upgrading pip
-%LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip -q
+%LocalAppData%\Programs\Python\Python311\python.exe -m pip install --upgrade pip
 echo Adding package requients
-%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r "%~dp0\requirements.txt" -q
-%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -i https://PySimpleGUI.net/install PySimpleGUI -q
+%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -r "%~dp0\requirements.txt"
+%LocalAppData%\Programs\Python\Python311\Scripts\pip.exe install -i https://PySimpleGUI.net/install PySimpleGUI
 echo Copying files
 if not exist %USERPROFILE%\ASoup md %USERPROFILE%\ASoup
 copy /y "%~dp0\alphasoup.py" %USERPROFILE%\ASoup
